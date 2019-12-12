0a50281b33e6d51922ea592f84e98aa845474ef7
Rutger ter Heide <s1114424@student.windesheim.nl>
2019-12-12 18:25:01 +0100

    Charts updated

--- client/pages/testing.vue
+++ client/pages/testing.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="small">
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
+  <div class="flex">
+    <line-chart class="p-8"
+      :chart-data="datacollection"
+      :options="options"/>
+    <bar-chart class="p-8" 
+      :chart-data="datatoday"
+      :options="options"/>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="fillData()">Randomize</button>
   </div>
 </template>
 
 <script>
   import LineChart from '~/components/charts/linechart.js'
+  import BarChart from '~/components/charts/barchart.js'
 
   export default {
     components: {
-      LineChart
+      LineChart,
+      BarChart
     },
     data () {
       return {
-        datacollection: null
+        datacollection: null,
+        datatoday: null
       }
     },
     mounted () {
@@ -23,16 +31,22 @@
     methods: {
       fillData () {
         this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
+          labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
           datasets: [
             {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }, {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
+              label: 'Times used in a week',
+              backgroundColor: '#BCDEFA',
+              data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+        this.datatoday = {
+          labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"],
+          datasets: [
+            {
+              label: 'Times used today',
+              backgroundColor: '#6574CD',
+              data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
             }
           ]
         }
