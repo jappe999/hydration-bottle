9f68018c4bdaf97629e372a7af786de8b03ec1f0
Jasper van der Linden <jappe@pop-os.localdomain>
2019-12-13 12:07:22 +0100

    feat: Show awesome charts

--- client/pages/device/_bottle.vue
+++ client/pages/device/_bottle.vue
@@ -5,8 +5,15 @@
       <small>{{ bottle.code }}</small>
     </div>
 
-    <app-card>
-      <app-card-content></app-card-content>
+    <app-card class="flex">
+      <app-card-content class="text-center border-r border-gray-900">
+        <h3 class="text-2xl font-bold">Times used in a week</h3>
+        <line-chart class="p-8" :chart-data="dataCollection" />
+      </app-card-content>
+      <app-card-content class="text-center">
+        <h3 class="text-2xl font-bold">Times used today</h3>
+        <bar-chart class="p-8" :chart-data="dataToday" />
+      </app-card-content>
     </app-card>
   </app-card-content>
 </template>
@@ -21,15 +28,91 @@ import * as types from '~/store/mutation-types'
   components: {
     AppCard: () => import('~/components/card/card.vue'),
     AppCardContent: () => import('~/components/card/card-content.vue'),
+    LineChart: () => import('~/components/charts/linechart.js'),
+    BarChart: () => import('~/components/charts/barchart.js'),
   },
 })
 export default class DevicePage extends Vue {
+  dataCollection = {
+    labels: [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: '#BCDEFA',
+        data: [
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+        ],
+      },
+    ],
+  }
+  dataToday = {
+    labels: [
+      '00:00',
+      '01:00',
+      '02:00',
+      '03:00',
+      '04:00',
+      '05:00',
+      '06:00',
+      '07:00',
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+      '20:00',
+      '21:00',
+      '22:00',
+      '23:00',
+    ],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: '#6574CD',
+        data: [
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+          this.getRandomInt(),
+        ],
+      },
+    ],
+  }
+
   @Getter('bottles/bottle') bottle: (id: string) => BottleView
 
   @Action('bottles/fetchBottle') fetchBottle: (id: string) => BottleView
 
   mounted() {
     this.fetchBottle(this.$route.params.bottle)
+  }
+
+  getRandomInt() {
+    return Math.floor(Math.random() * (50 - 5 + 1)) + 5
   }
 }
 </script>
